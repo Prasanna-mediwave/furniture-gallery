83394339b023133c0b979ede2dab338e35a7f09b
Prasanna-mediwave <prasanna@mindwaveventures.com>
2023-06-07 15:09:53 +0530

    tailwind config completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primaryBG: "#FFFFFF",
+        primaryContent: "#313131",
+        grey: "#E8E8E8",
+        sandleYellow: "#D1A800",
+        red: "#C12929",
+        pages: "#EAEAEA",
+        bColor: "#F2F2F2",
+      },
+      fontSize: {
+        font_sm: "18px",
+        font_md: "26px",
+        font_lg: "38px",
+        font_xl: "50px",
+      },
+      fontFamily: {
+        primaryFont: "'Raleway', sans-serif",
+        secondaryFont: "'Poppins', sans-serif",
+      },
+      boxShadow: {
+        default: "0px 8px 16px #0000000A",
+        priceCard: "0px 16px 12px #00000014",
       },
     },
   },
   plugins: [],
-}
+};
